b645506f1a547cfd4a17296254addc609a9544b5
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-06-17 15:46:03 +0200

    Add asBoolean

---
+++ src/sanitize/asBoolean.ts
@@ -0,0 +1,18 @@
+import { Either, Sanitizer } from './sanitizer'
+
+const BOOLEAN_REGEX = /^(true|false)$/
+
+const isBoolean = (value: unknown): value is boolean =>
+  typeof value === 'boolean'
+
+const isBooleanString = (value: unknown): value is string =>
+  typeof value === 'string' && BOOLEAN_REGEX.test(value)
+
+export const asBoolean: Sanitizer<boolean> = (value, path) => {
+  if (isBoolean(value)) {
+    return Either.right(value)
+  } else if (isBooleanString(value)) {
+    return Either.right(value === 'true')
+  }
+  return Either.left([{ path, expected: 'boolean' }])
+}
---
+++ test/sanitize/asBoolean.test.ts
@@ -0,0 +1,34 @@
+import { expect } from 'chai'
+import { asBoolean } from '../../src/sanitize/asBoolean'
+import { Either } from '../../src/sanitize/sanitizer'
+
+describe('asBoolean', () => {
+  it('sanitizes strings containing "true"', async () => {
+    const result = asBoolean('true', '')
+    expect(result).to.deep.equal(Either.right(true))
+  })
+
+  it('sanitizes strings containing "false"', async () => {
+    const result = asBoolean('false', '')
+    expect(result).to.deep.equal(Either.right(false))
+  })
+
+  it('does not accept non-boolean strings', async () => {
+    const result = asBoolean('12abc', 'path')
+    expect(result).to.deep.equal(
+      Either.left([{ path: 'path', expected: 'boolean' }])
+    )
+  })
+
+  it('sanitizes booleans', async () => {
+    const result = asBoolean(true, '')
+    expect(result).to.deep.equal(Either.right(true))
+  })
+
+  it('does not accept types other than boolean or string', async () => {
+    const result = asBoolean(13, 'path')
+    expect(result).to.deep.equal(
+      Either.left([{ path: 'path', expected: 'boolean' }])
+    )
+  })
+})
