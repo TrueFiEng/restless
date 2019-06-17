a5bdad75d8d39e8a472ec24a36ce8b55dcc85c26
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-06-17 15:42:28 +0200

    Add asArray

---
+++ src/sanitize/asArray.ts
@@ -0,0 +1,21 @@
+import { Either, Sanitizer, SanitizerError } from './sanitizer'
+
+export const asArray = <T> (sanitizer: Sanitizer<T>): Sanitizer<T[]> =>
+  (value, path) => {
+    if (!Array.isArray(value)) {
+      return Either.left([{ path, expected: 'array' }])
+    }
+    const results: T[] = []
+    const errors: SanitizerError[] = []
+    for (let i = 0; i < value.length; i++) {
+      const result = sanitizer(value[i], `${path}[${i}]`)
+      if (Either.isRight(result)) {
+        results.push(result.right)
+      } else {
+        errors.push(...result.left)
+      }
+    }
+    return errors.length > 0
+      ? Either.left(errors)
+      : Either.right(results)
+  }
--- src/sanitize/asObject.ts
+++ src/sanitize/asObject.ts
@@ -5,24 +5,23 @@ type Schema<T> = {
 }
 
 export const asObject = <T extends object> (schema: Schema<T>): Sanitizer<T> =>
-  (data, path) => {
-    if (typeof data !== 'object' || data === null) {
+  (value, path) => {
+    if (typeof value !== 'object' || value === null) {
       return Either.left([{ path, expected: 'object' }])
     }
-    const value: T = {} as any
+    const results: T = {} as any
     const errors: SanitizerError[] = []
     for (const key in schema) {
       if (Object.hasOwnProperty.call(schema, key)) {
-        const result = schema[key]((data as T)[key], `${path}.${key}`)
+        const result = schema[key]((value as T)[key], `${path}.${key}`)
         if (Either.isRight(result)) {
-          value[key] = result.right
+          results[key] = result.right
         } else {
           errors.push(...result.left)
         }
       }
     }
-    if (errors.length > 0) {
-      return Either.left(errors)
-    }
-    return Either.right(value)
+    return errors.length > 0
+      ? Either.left(errors)
+      : Either.right(results)
   }
