0f8d1ac6daa557be9cf394079ed848c449db4690
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-06-17 16:03:52 +0200

    add asChecked

---
+++ src/sanitize/asChecked.ts
@@ -0,0 +1,13 @@
+import { Either, Sanitizer } from './sanitizer'
+
+export const asChecked = <T> (
+  sanitizer: Sanitizer<T>,
+  predicate: (value: T) => boolean,
+  expected: string = 'custom logic'
+): Sanitizer<T> => (value, path) => {
+  const result = sanitizer(value, path)
+  if (Either.isRight(result) && !predicate(result.right)) {
+    return Either.left([{ path, expected }])
+  }
+  return result
+}
