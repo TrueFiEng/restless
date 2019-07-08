1cb035525cbc9c1d125a1a082d27895cfbeddc8e
Marik-D <maretskii@gmail.com>
2019-07-08 16:52:24 +0200

    Add buffer response

--- src/response.ts
+++ src/response.ts
@@ -3,5 +3,13 @@ import { Response as ExpressResponse } from 'express'
 export type Response = (res: ExpressResponse) => void
 
 export const responseOf = (data: any, status = 200): Response => res => {
-  res.status(status).json(data)
+  res
+    .status(status)
+    .json(data)
+}
+
+export const responseOfBuffer = (type: string, data: Buffer, status = 200): Response => res => {
+  res.status(status)
+    .type(type)
+    .send(data)
 }
--- test/response.test.ts
+++ test/response.test.ts
@@ -2,30 +2,49 @@ import chai, { expect } from 'chai'
 import chaiHttp from 'chai-http'
 import express from 'express'
 import { asyncHandler } from '../src/asyncHandler'
-import { responseOf } from '../src/response'
+import { responseOf, responseOfBuffer, Response } from '../src/response'
 
 chai.use(chaiHttp)
 
 describe('response', () => {
   describe('responseOf', () => {
     it('returns status 200 by default', async () => {
-      const app = express()
-      app.get('/', asyncHandler(() => responseOf({ a: 1, b: 'xd' })))
-
-      const response = await chai.request(app).get('/').send()
+      const response = await testResponse(responseOf({ a: 1, b: 'xd' }))
 
       expect(response.status).to.equal(200)
       expect(response.body).to.deep.equal({ a: 1, b: 'xd' })
     })
 
     it('returns the provided status when passed', async () => {
-      const app = express()
-      app.get('/', asyncHandler(() => responseOf('data', 404)))
-
-      const response = await chai.request(app).get('/').send()
+      const response = await testResponse(responseOf('data', 404))
 
       expect(response.status).to.equal(404)
       expect(response.body).to.equal('data')
     })
   })
+
+  describe('responseOfBuffer', () => {
+    it('returns status 200 by default', async () => {
+      const response = await testResponse(responseOfBuffer('png', Buffer.from('ABC', 'ascii')))
+
+      expect(response.status).to.equal(200)
+      expect(response.type).to.equal('image/png')
+      expect(response.body).to.deep.equal(Buffer.from('ABC', 'ascii'))
+    })
+
+    it('returns the provided status when passed', async () => {
+      const response = await testResponse(responseOfBuffer('png', Buffer.from('ABC', 'ascii'), 404))
+
+      expect(response.status).to.equal(404)
+      expect(response.type).to.equal('image/png')
+      expect(response.body).to.deep.equal(Buffer.from('ABC', 'ascii'))
+    })
+  })
 })
+
+async function testResponse(response: Response) {
+  const app = express()
+  app.get('/', asyncHandler(() => response))
+
+  return chai.request(app).get('/').send()
+}
