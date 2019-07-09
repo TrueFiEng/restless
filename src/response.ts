55b54854c27b3df62f47f35b4f554795d26aa717
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-07-09 09:29:08 +0200

    Fix linter

--- src/response.ts
+++ src/response.ts
@@ -3,12 +3,12 @@ import { Response } from 'express'
 export type ResponseFunction = (res: Response) => void
 
 export const responseOf = (data: any, status = 200): ResponseFunction =>
-  res => res
+  (res) => res
     .status(status)
     .json(data)
 
 export const responseOfBuffer = (type: string, data: Buffer, status = 200): ResponseFunction =>
-  res => res
+  (res) => res
     .status(status)
     .type(type)
     .send(data)
