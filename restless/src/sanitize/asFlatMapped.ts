9bc7dea994b222bf2ca59507204168ee10aa489f
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-07-19 09:29:01 +0200

    add asMapped and asFlatMapped

---
+++ restless/src/sanitize/asFlatMapped.ts
@@ -0,0 +1,12 @@
+import { Either, Sanitizer, SanitizerFailure } from './sanitizer'
+
+export const asFlatMapped = <T, U> (
+  sanitizer: Sanitizer<T>,
+  flatMapFn: (value: T, path: string) => Either<SanitizerFailure[], U>,
+): Sanitizer<U> => (value, path) => {
+  const result = sanitizer(value, path)
+  if (Either.isRight(result)) {
+    return flatMapFn(result.right, path)
+  }
+  return result
+}
