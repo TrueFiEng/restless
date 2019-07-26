2df22c47438343d21204f20c7f1cc0b601abb7b7
Marik-D <maretskii@gmail.com>
2019-07-26 16:48:07 +0200

    Add withErrorMessage

---
+++ restless/src/sanitize/withErrorMessage.ts
@@ -0,0 +1,15 @@
+import { Sanitizer, Either } from "./sanitizer";
+
+export const withErrorMessage = <T>(
+  sanitizer: Sanitizer<T>,
+  expected: string,
+): Sanitizer<T> => {
+  return (value, path) => {
+    const result = sanitizer(value, path)
+    if (Either.isLeft(result)) {
+      return Either.left([{ expected, path }])
+    } else {
+      return result
+    }
+  }
+}
---
+++ restless/test/sanitize/withErrorMessage.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import { asNumber, asObject, withErrorMessage, Either } from "../../src";
+
+describe('withErrorMessage', () => {
+  it('returns set error message if sanitizer fails', () => {
+    const sanitizer = withErrorMessage(asObject({ a: asNumber }), 'foo')
+    const res = { a: 'not a number' }
+
+    expect(sanitizer(res, 'path')).to.equal(Either.left([{ expected: 'foo', path: 'path' }]))
+  })
+
+  it('returns data if sanitization succeeded', () => {
+    const sanitizer = withErrorMessage(asObject({ a: asNumber }), 'foo')
+    const res = { a: 42 }
+
+    expect(sanitizer(res, 'path')).to.equal(Either.right(42))
+  })
+})
