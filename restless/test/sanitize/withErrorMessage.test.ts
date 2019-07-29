e78d1a0c8126fd065bf9697ff5ae5e797c3b3543
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-07-29 09:20:16 +0200

    Fix failing tests

--- restless/test/sanitize/withErrorMessage.test.ts
+++ restless/test/sanitize/withErrorMessage.test.ts
@@ -5,7 +5,7 @@ describe('withErrorMessage', () => {
   it('returns set error message if sanitizer fails', () => {
     const sanitizer = withErrorMessage(asObject({ a: asNumber }), 'foo')
     const result = sanitizer({ a: 'not a number' }, 'path')
-    expect(result).to.equal(
+    expect(result).to.deep.equal(
       Either.left([{ path: 'path', expected: 'foo' }])
     )
   })
@@ -13,6 +13,6 @@ describe('withErrorMessage', () => {
   it('returns data if sanitization succeeded', () => {
     const sanitizer = withErrorMessage(asObject({ a: asNumber }), 'foo')
     const result = sanitizer({ a: 42 }, 'path')
-    expect(result).to.equal(Either.right({ a: 42 }))
+    expect(result).to.deep.equal(Either.right({ a: 42 }))
   })
 })
