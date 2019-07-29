e3fdc2bb0ec73e9a07008b9cd10fab60d8dfa80b
Marik-D <35851437+Marik-D@users.noreply.github.com>
2019-07-29 09:21:01 +0200

    Add withErrorMessage (#13)
    * Add withErrorMessage
    
    * Lint
    
    * CR fix
    
    * CR fix
    
    * Fix failing tests

---
+++ restless/test/sanitize/withErrorMessage.test.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import { asNumber, asObject, Either, withErrorMessage } from '../../src'
+
+describe('withErrorMessage', () => {
+  it('returns set error message if sanitizer fails', () => {
+    const sanitizer = withErrorMessage(asObject({ a: asNumber }), 'foo')
+    const result = sanitizer({ a: 'not a number' }, 'path')
+    expect(result).to.deep.equal(
+      Either.left([{ path: 'path', expected: 'foo' }])
+    )
+  })
+
+  it('returns data if sanitization succeeded', () => {
+    const sanitizer = withErrorMessage(asObject({ a: asNumber }), 'foo')
+    const result = sanitizer({ a: 42 }, 'path')
+    expect(result).to.deep.equal(Either.right({ a: 42 }))
+  })
+})
