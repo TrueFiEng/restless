44dcb9f30e9ff24b55760c0a5987024688adf38c
Piotr Szlachciak <szlachciak.piotr@gmail.com>
2019-08-08 10:20:27 +0200

    Update ethereum package

--- ethereum/src/asEthAddress.ts
+++ ethereum/src/asEthAddress.ts
@@ -1,11 +1,11 @@
-import { Either, Sanitizer } from '@restless/sanitizers'
+import { Result, Sanitizer } from '@restless/sanitizers'
 import { utils } from 'ethers'
 
 export const asEthAddress: Sanitizer<string> = (value, path) => {
   try {
     if (typeof value === 'string') {
-      return Either.right(utils.getAddress(value))
+      return Result.ok(utils.getAddress(value))
     }
   } catch {} // tslint:disable-line
-  return Either.left([{ path, expected: 'ethereum address' }])
+  return Result.error([{ path, expected: 'ethereum address' }])
 }
--- ethereum/test/asEthAddress.test.ts
+++ ethereum/test/asEthAddress.test.ts
@@ -1,4 +1,4 @@
-import { Either } from '@restless/sanitizers'
+import { Result } from '@restless/sanitizers'
 import { expect } from 'chai'
 import { Wallet } from 'ethers'
 import { asEthAddress } from '../src/asEthAddress'
@@ -7,20 +7,20 @@ describe('asEthAddress', () => {
   it('sanitizes strings that are valid ethereum addresses', async () => {
     const { address } = Wallet.createRandom()
     const result = asEthAddress(address, '')
-    expect(result).to.deep.equal(Either.right(address))
+    expect(result).to.deep.equal(Result.ok(address))
   })
 
   it('keeps the string normalized', async () => {
     const { address } = Wallet.createRandom()
     const result = asEthAddress(address.toLowerCase(), '')
-    expect(result).to.deep.equal(Either.right(address))
+    expect(result).to.deep.equal(Result.ok(address))
   })
 
   it('does not accept invalid ethereum addresses', async () => {
     const INVALID_ADDRESS = '0x7a4EdC939038dbB58befD7028b1625DFf2ca58b8'
     const result = asEthAddress(INVALID_ADDRESS, 'path')
     expect(result).to.deep.equal(
-      Either.left([{ path: 'path', expected: 'ethereum address' }])
+      Result.error([{ path: 'path', expected: 'ethereum address' }])
     )
   })
 
@@ -28,14 +28,14 @@ describe('asEthAddress', () => {
     const NOT_AN_ADDRESS = 'bla bla bla'
     const result = asEthAddress(NOT_AN_ADDRESS, 'path')
     expect(result).to.deep.equal(
-      Either.left([{ path: 'path', expected: 'ethereum address' }])
+      Result.error([{ path: 'path', expected: 'ethereum address' }])
     )
   })
 
   it('does not accept non strings', async () => {
     const result = asEthAddress(123, 'path')
     expect(result).to.deep.equal(
-      Either.left([{ path: 'path', expected: 'ethereum address' }])
+      Result.error([{ path: 'path', expected: 'ethereum address' }])
     )
   })
 })
