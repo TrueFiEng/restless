97ad5cc6f4c798a53c3dbae45d8e8ecdef57be4b
Marik-D <35851437+Marik-D@users.noreply.github.com>
2020-02-26 13:29:32 +0100

    Add asEnum sanitizer (#35)
    * Add asEnum sanitizer
    
    * Update sanitizers/test/asEnum.test.ts
    
    Co-authored-by: Piotr Szlachciak <szlachciak.piotr@gmail.com>

---
+++ sanitizers/test/asEnum.test.ts
@@ -0,0 +1,21 @@
+import { expect } from 'chai'
+import { asEnum, Result, Sanitizer } from '../src'
+
+describe('asEnum', () => {
+  it('enum with 2 strings', () => {
+    const sanitizer: Sanitizer<'foo' | 'bar'> = asEnum<'foo' | 'bar'>(['foo', 'bar'], 'enum')
+    expect(sanitizer('foo', '')).to.deep.eq(Result.ok('foo'))
+    expect(sanitizer('bar', '')).to.deep.eq(Result.ok('bar'))
+    expect(sanitizer('baz', '')).to.deep.eq(Result.error([{ path: '', expected: 'enum' }]))
+    expect(sanitizer(3, '')).to.deep.eq(Result.error([{ path: '', expected: 'enum' }]))
+  })
+
+  it('enum with number + string', () => {
+    const sanitizer = asEnum(['foo', 23], 'enum')
+    expect(sanitizer('foo', '')).to.deep.eq(Result.ok('foo'))
+    expect(sanitizer(23, '')).to.deep.eq(Result.ok(23))
+    expect(sanitizer('baz', '')).to.deep.eq(Result.error([{ path: '', expected: 'enum' }]))
+    expect(sanitizer(3, '')).to.deep.eq(Result.error([{ path: '', expected: 'enum' }]))
+    expect(sanitizer(null, '')).to.deep.eq(Result.error([{ path: '', expected: 'enum' }]))
+  })
+})
