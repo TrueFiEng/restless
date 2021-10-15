b2e66b408385b9cc674efdc95770bed898822f8f
Dmytro Maretskyi <35851437+Marik-D@users.noreply.github.com>
2021-10-15 15:05:02 +0300

    Update sanitizers/src/asNullable.ts

--- sanitizers/src/asNullable.ts
+++ sanitizers/src/asNullable.ts
@@ -4,5 +4,5 @@ export const asNullable = <T>(sanitizer: Sanitizer<T>) : Sanitizer<T | null> =>
   (value, path) => value === null
     ? Result.ok(null)
       : value === undefined 
-      ? Result.error([{ path: 'path', expected: 'not undefined' }])
+      ? Result.error([{ path, expected: 'not undefined' }])
         : sanitizer(value, path)
